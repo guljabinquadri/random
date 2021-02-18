88e518953f283cc722ea014c75012095f91e1e3c
guljabinquadri <78896565+guljabinquadri@users.noreply.github.com>
2021-02-19 00:29:57 +0530

    init commit

---
+++ random_1.java
@@ -0,0 +1,38 @@
+package loops;
+
+public class LoopsConcepts {
+
+	public static void main(String[] args) {
+		/*Quick quiz/(while loop)
+				int i = 100;
+				while (i<=200)
+				{
+					System.out.println(i);
+					i++;
+				} 
+				//do while loop
+				int i =1;
+						do {
+							System.out.println(i);
+							i++;
+
+						}
+				while(i<=10); */
+		int n = 5;
+		for(int i =0; i<n; i++)
+		{
+			System.out.println(2*i+1);
+		}
+		//Decrementing 
+		for(int i =10;i!=0; i--)
+		{
+			System.out.println(i);
+		}
+	for(int i =10;i>0;i--)
+	{
+		System.out.println(i);
+	}
+
+	}
+
+}
